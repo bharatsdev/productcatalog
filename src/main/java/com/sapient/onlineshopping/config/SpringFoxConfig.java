ca0c5e59d4c1834ff467b912f6d5f0fa22ede63d
Bharath Singh <b.singh@tatweer-co.ae>
2020-01-08 18:55:36 +0400

    Add Swagger

--- src/main/java/com/sapient/onlineshopping/config/SpringFoxConfig.java
+++ src/main/java/com/sapient/onlineshopping/config/SpringFoxConfig.java
@@ -3,6 +3,12 @@
  */
 package com.sapient.onlineshopping.config;
 
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -11,5 +17,9 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @EnableSwagger2
 public class SpringFoxConfig {
-
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any()).build();
+	}
 }
